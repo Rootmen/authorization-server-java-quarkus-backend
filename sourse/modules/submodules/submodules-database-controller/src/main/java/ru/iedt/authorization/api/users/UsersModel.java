4a84635960c20bb323ca3cec4259aefb0d851075
Rootmen <10poma10@mail.ru>
2024-01-23 21:02:35 +0300

    fix code style

--- sourse/modules/submodules/submodules-database-controller/src/main/java/ru/iedt/authorization/api/users/UsersModel.java
+++ sourse/modules/submodules/submodules-database-controller/src/main/java/ru/iedt/authorization/api/users/UsersModel.java
@@ -15,84 +15,84 @@ import ru.iedt.database.request.controller.parameter.ParameterInput;
 
 @ApplicationScoped
 public class UsersModel {
-    @Inject
-    DatabaseController databaseController;
+	@Inject
+	DatabaseController databaseController;
 
-    public Uni<UserInfo> getUserInfo(UUID userId, PgPool client) {
-        HashMap<String, ParameterInput> parameters = new HashMap<>();
-        parameters.put("USER_UUID", new ParameterInput("USER_UUID", userId.toString()));
-        return databaseController
-                .runningQuerySet("users_model", "GET_USER_INFO_FROM_ID", parameters, client)
-                .onItem()
-                .transform(element -> element.get(0).get("main"))
-                .onItem()
-                .transform(RowSet::iterator)
-                .onItem()
-                .transform(iterator -> iterator.hasNext() ? UserInfo.from(iterator.next()) : null);
-    }
+	public Uni<UserInfo> getUserInfo(UUID userId, PgPool client) {
+		HashMap<String, ParameterInput> parameters = new HashMap<>();
+		parameters.put("USER_UUID", new ParameterInput("USER_UUID", userId.toString()));
+		return databaseController
+				.runningQuerySet("users_model", "GET_USER_INFO_FROM_ID", parameters, client)
+				.onItem()
+				.transform(element -> element.get(0).get("main"))
+				.onItem()
+				.transform(RowSet::iterator)
+				.onItem()
+				.transform(iterator -> iterator.hasNext() ? UserInfo.from(iterator.next()) : null);
+	}
 
-    public Multi<UserInfo> getAllUserInfo(PgPool client) {
-        return databaseController
-                .runningQuerySet("users_model", "GET_ALL_USER_INFO", new HashMap<>(), client)
-                .onItem()
-                .transform(element -> element.get(0).get("main"))
-                .onItem()
-                .transformToMulti(set -> Multi.createFrom().iterable(set))
-                .onItem()
-                .transform(UserInfo::from);
-    }
+	public Multi<UserInfo> getAllUserInfo(PgPool client) {
+		return databaseController
+				.runningQuerySet("users_model", "GET_USER_ACCOUNT", new HashMap<>(), client)
+				.onItem()
+				.transform(element -> element.get(0).get("main"))
+				.onItem()
+				.transformToMulti(set -> Multi.createFrom().iterable(set))
+				.onItem()
+				.transform(UserInfo::from);
+	}
 
-    public Multi<UserAccount> getAllUserAccount(PgPool client) {
-        return databaseController
-                .runningQuerySet("users_model", "GET_USER_ACCOUNT", new HashMap<>(), client)
-                .onItem()
-                .transform(element -> element.get(0).get("main"))
-                .onItem()
-                .transformToMulti(set -> Multi.createFrom().iterable(set))
-                .onItem()
-                .transform(UserAccount::from);
-    }
+	public Multi<UserAccount> getAllUserAccount(PgPool client) {
+		return databaseController
+				.runningQuerySet("users_model", "GET_USER_ACCOUNT", new HashMap<>(), client)
+				.onItem()
+				.transform(element -> element.get(0).get("main"))
+				.onItem()
+				.transformToMulti(set -> Multi.createFrom().iterable(set))
+				.onItem()
+				.transform(UserAccount::from);
+	}
 
-    public Uni<UserAccount> getUserAccount(String username, PgPool client) {
-        HashMap<String, ParameterInput> parameters = new HashMap<>();
-        parameters.put("USER_NAME", new ParameterInput("USER_NAME", username));
-        return databaseController
-                .runningQuerySet("users_model", "GET_USER_ACCOUNT", parameters, client)
-                .onItem()
-                .transform(element -> element.get(0).get("main"))
-                .onItem()
-                .transform(RowSet::iterator)
-                .onItem()
-                .transform(iterator -> iterator.hasNext() ? UserAccount.from(iterator.next()) : null);
-    }
+	public Uni<UserAccount> getUserAccount(String username, PgPool client) {
+		HashMap<String, ParameterInput> parameters = new HashMap<>();
+		parameters.put("USER_NAME", new ParameterInput("USER_NAME", username));
+		return databaseController
+				.runningQuerySet("users_model", "GET_USER_ACCOUNT", parameters, client)
+				.onItem()
+				.transform(element -> element.get(0).get("main"))
+				.onItem()
+				.transform(RowSet::iterator)
+				.onItem()
+				.transform(iterator -> iterator.hasNext() ? UserAccount.from(iterator.next()) : null);
+	}
 
-    public Uni<UserAccount> getUserAccount(UUID userId, PgPool client) {
-        HashMap<String, ParameterInput> parameters = new HashMap<>();
-        parameters.put("USER_UUID", new ParameterInput("USER_UUID", userId.toString()));
-        return databaseController
-                .runningQuerySet("users_model", "GET_USER_ACCOUNT", parameters, client)
-                .onItem()
-                .transform(element -> element.get(0).get("main"))
-                .onItem()
-                .transform(RowSet::iterator)
-                .onItem()
-                .transform(iterator -> iterator.hasNext() ? UserAccount.from(iterator.next()) : null);
-    }
+	public Uni<UserAccount> getUserAccount(UUID userId, PgPool client) {
+		HashMap<String, ParameterInput> parameters = new HashMap<>();
+		parameters.put("USER_UUID", new ParameterInput("USER_UUID", userId.toString()));
+		return databaseController
+				.runningQuerySet("users_model", "GET_USER_ACCOUNT", parameters, client)
+				.onItem()
+				.transform(element -> element.get(0).get("main"))
+				.onItem()
+				.transform(RowSet::iterator)
+				.onItem()
+				.transform(iterator -> iterator.hasNext() ? UserAccount.from(iterator.next()) : null);
+	}
 
-    public Uni<UserAccount> addUserAccount(
-            String username, String userMail, String passwordVerifier, String salt, PgPool client) {
-        HashMap<String, ParameterInput> parameters = new HashMap<>();
-        parameters.put("USER_NAME", new ParameterInput("USER_NAME", username));
-        parameters.put("USER_MAIL", new ParameterInput("USER_MAIL", userMail));
-        parameters.put("PASSWORD_VERIFIER", new ParameterInput("PASSWORD_VERIFIER", passwordVerifier));
-        parameters.put("SALT", new ParameterInput("SALT", salt));
-        return databaseController
-                .runningQuerySet("users_model", "ADD_USER_ACCOUNT", parameters, client)
-                .onItem()
-                .transform(element -> element.get(0).get("main"))
-                .onItem()
-                .transform(RowSet::iterator)
-                .onItem()
-                .transform(iterator -> iterator.hasNext() ? UserAccount.from(iterator.next()) : null);
-    }
+	public Uni<UserAccount> addUserAccount(
+			String username, String userMail, String passwordVerifier, String salt, PgPool client) {
+		HashMap<String, ParameterInput> parameters = new HashMap<>();
+		parameters.put("USER_NAME", new ParameterInput("USER_NAME", username));
+		parameters.put("USER_MAIL", new ParameterInput("USER_MAIL", userMail));
+		parameters.put("PASSWORD_VERIFIER", new ParameterInput("PASSWORD_VERIFIER", passwordVerifier));
+		parameters.put("SALT", new ParameterInput("SALT", salt));
+		return databaseController
+				.runningQuerySet("users_model", "ADD_USER_ACCOUNT", parameters, client)
+				.onItem()
+				.transform(element -> element.get(0).get("main"))
+				.onItem()
+				.transform(RowSet::iterator)
+				.onItem()
+				.transform(iterator -> iterator.hasNext() ? UserAccount.from(iterator.next()) : null);
+	}
 }
