062f65eebc7c29b3dc6b7024039cdc48220306f3
Rootmen <10poma10@mail.ru>
2024-03-21 20:44:08 +0300

    Update RandomString.java

--- source/modules/submodules/submodules-cryptography/src/main/java/ru/iedt/authorization/crypto/RandomString.java
+++ source/modules/submodules/submodules-cryptography/src/main/java/ru/iedt/authorization/crypto/RandomString.java
@@ -1,17 +1,18 @@
 package ru.iedt.authorization.crypto;
 
-
 import java.security.SecureRandom;
 
 public class RandomString {
-    static SecureRandom random = new SecureRandom();
+
+    static SecureRandom random = null;
     static String group = "0123456789abcdef";
 
     public static String getRandomString(int size) {
-       return getRandomString(size, group);
+        return getRandomString(size, group);
     }
 
     public static String getRandomString(int size, String group) {
+        if (random == null) random = new SecureRandom();
         StringBuilder result = new StringBuilder(size);
         for (int g = 0; g < size; g++) {
             int ch = random.nextInt(0, group.length());
@@ -19,5 +20,4 @@ public class RandomString {
         }
         return result.toString();
     }
-
 }
